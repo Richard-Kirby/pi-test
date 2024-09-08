3c1f58e5fb6d7ca62bf301178d8b56c99f203bc9
Richard Kirby <richard.james.kirby@gmail.com>
2024-09-08 19:15:16 +0100

    Working version

--- test.py
+++ test.py
@@ -14,15 +14,15 @@ pinout_wiring = [
     (17, 0, 0),    (18, 24, 27), # odd 3v3
     (19, 10, 8),   (20, 0, 0), # GND
     (21, 9, 7),    (22, 25, 22),
-    (23, 11, 0),   (24, 8, 10),
+    (23, 11, 12),   (24, 8, 10),
     (25, 0, 0),    (26, 7, 0),# Odd GND
     (27, 0, 0),    (28, 0, 0), #GPIO0/1 - not usable.
-    (29, 5, 0),    (30, 0, 0), # GND
-    (31, 6, 0),    (32, 12, 0),
-    (33, 13, 0),   (34, 0, 0), # GND
-    (35, 19, 0),   (36, 16, 0),
-    (37, 26, 0),   (38, 20, 0),
-    (39, 0, 0),    (40, 21, 0)] # GND
+    (29, 5, 16),    (30, 0, 0), # GND
+    (31, 6, 20),    (32, 12, 11),
+    (33, 13, 21),   (34, 0, 0), # GND
+    (35, 19, 26),   (36, 16, 5),
+    (37, 26, 19),   (38, 20, 6),
+    (39, 0, 0),    (40, 21, 13)] # GND
 
 for i in pinout_wiring:
     print(i)
@@ -49,15 +49,46 @@ pi = pigpio.pi()
 
 import RPi.GPIO as GPIO
 
+
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-GPIO.output(2, 0)
-GPIO.setup(14, GPIO.IN)
-print(f"res {GPIO.input(14)}")
-time.sleep(1)
-GPIO.output(2, 1)
-print(f"res {GPIO.input(14)}")
-time.sleep(1)
+
+class Test:
+    def __init__(self, pin_num, out_pin, in_pin):
+        self.pin_num = pin_num
+        self.out_pin = out_pin
+        self.in_pin = in_pin
+        print(f"test {self.pin_num}, {self.out_pin}, {self.in_pin}")
+
+    def test(self):
+        result = [False, False, False]
+        GPIO.setup(self.out_pin, GPIO.OUT)
+        GPIO.setup(self.in_pin, GPIO.IN)
+
+        GPIO.output(self.out_pin, 0)
+        if GPIO.input(self.in_pin) == 0:
+            result[0] = True
+
+        print(f"pin {self.pin_num} {self.out_pin} {self.in_pin} {GPIO.input(self.in_pin)}")
+        time.sleep(0.2)
+
+        GPIO.output(self.out_pin, 1)
+        if GPIO.input(self.in_pin) == 1:
+            result[1] = True
+
+        print(f"pin {self.pin_num} {self.out_pin} {self.in_pin} {GPIO.input(self.in_pin)}")
+        time.sleep(0.2)
+
+        GPIO.output(self.out_pin, 0)
+        if GPIO.input(self.in_pin) == 0:
+            result[2] = True
+
+        print(f"pin {self.pin_num} {self.out_pin} {self.in_pin} {GPIO.input(self.in_pin)}")
+        time.sleep(0.2)
+
+        print(result)
+        return result
+
+
 
 """
 pi.write(2, 0)
@@ -68,9 +99,29 @@ print(f"res {pi.read(14)}")
 time.sleep(1)
 """
 
+tests =[]
+results =[]
+
 for i in range (40):
+    if pinout_wiring[i][1] != 0 and pinout_wiring[i][2] != 0:
+        test = Test(pinout_wiring[i][0], pinout_wiring[i][1], pinout_wiring[i][2])
+        tests.append(test)
+
+overall_pass = True
+for test in tests:
+    result = test.test()
+
+    if result != [True, True, True]:
+        overall_pass = False
+
+    results.append(result)
+
+print(results)
+
+print(f"Overall Results {overall_pass}")
+
+"""
     # print(f"testing pin, from, to: {pinout_wiring[i][0]}, {pinout_wiring[i][1]}, {pinout_wiring[i][2]}")
-    if pinout_wiring[i][1] != 0:
         pi.write(pinout_wiring[i][1], 0)
 
         print(f"  pin, from, to, results: {pinout_wiring[i][0]}, {pinout_wiring[i][1]}, {pinout_wiring[i][2]}, "
@@ -96,5 +147,5 @@ for i in range(2, 28):
     print(results)
     time.sleep(2)
     pi.write(i, 0)
-
+"""
 
